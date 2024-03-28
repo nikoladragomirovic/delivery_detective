b471517c5dcabd7068745c61a524bad094d120b5
Nikola Dragomirović <132782535+nikoladragomirovic@users.noreply.github.com>
2024-03-28 23:59:00 +0100

    Refactored Parser

--- backend/parser.py
+++ backend/parser.py
@@ -3,22 +3,40 @@ from pymongo import MongoClient
 
 def parse(data, service):
     parsed = []
+    
     if isinstance(data, dict):
-        if service == "glovo":
-            if "name" in data and "primeAvailable" in data and "slug" in data:
-                parsed.append({"name": data["name"], "services": [{"name": "Glovo", "link": "https://glovoapp.com/rs/en/novi-sad/"+data["slug"], "free_delivery": data["primeAvailable"]}]})
-        elif service == "wolt":
-            if "name" in data and "show_wolt_plus" in data and "slug" in data:
-                parsed.append({"name": data["name"], "services": [{"name": "Wolt", "link": "https://wolt.com/en/srb/novi_sad/restaurant/"+data["slug"], "free_delivery": data["show_wolt_plus"]}]})
+        if service == "glovo" and all(key in data for key in ("name", "primeAvailable", "slug", "serviceFee")):
+            parsed.append({
+                "name": data["name"],
+                "services": [{
+                    "name": "Glovo",
+                    "link": "https://glovoapp.com/rs/en/novi-sad/" + data["slug"],
+                    "price": "{:.2f}".format(data["serviceFee"]),
+                    "free_delivery": data["primeAvailable"]
+                }]
+            })
+        elif service == "wolt" and all(key in data for key in ("name", "show_wolt_plus", "slug", "delivery_price_int")):
+            parsed.append({
+                "name": data["name"],
+                "services": [{
+                    "name": "Wolt",
+                    "link": "https://wolt.com/en/srb/novi_sad/restaurant/" + data["slug"],
+                    "price": "{:.2f}".format(float(data["delivery_price_int"]) / 100),
+                    "free_delivery": data["show_wolt_plus"]
+                }]
+            })
+        
         for value in data.values():
             parsed.extend(parse(value, service))
+            
     elif isinstance(data, list):
         for item in data:
-            parsed.extend(parse(item, service)) 
+            parsed.extend(parse(item, service))
+    
     return parsed
 
-def merge(glovo_data, wolt_data):
-    combined_data = glovo_data + wolt_data
+def merge(data_list):
+    combined_data = [item for sublist in data_list for item in sublist]
     merged_data = {}
 
     for item in combined_data:
@@ -42,33 +60,41 @@ def db_insert(data):
     restaurants_collection.drop()
     restaurants_collection.insert_many(data)
 
-glovo_url = 'https://api.glovoapp.com/v3/feeds/categories/1'
-wolt_url = 'https://consumer-api.wolt.com/v1/pages/restaurants'
+def get_data():
+    wolt = {
+        'url': 'https://consumer-api.wolt.com/v1/pages/restaurants',
+        'params': {
+            'lat': '45.252317',
+            'lon': '19.8335496',
+        },
+        'headers': {
+            'Authorization': 'Bearer eyJhbGciOiJFUzI1NiIsImtpZCI6IjY1ZjM5MGY3MDAwMDAwMDAwMDAwMDAwMCIsInR5cCI6IngudXNlcitqd3QifQ.eyJhdWQiOlsiY29ycG9yYXRlLXBvcnRhbC1hcGkiLCJyZXR1cm5zLWFwaSIsImNvdXJpZXJjbGllbnQiLCJwYXltZW50cy10aXBzLXNlcnZpY2UiLCJvcmRlci10cmFja2luZyIsImNvbnN1bWVyLWFzc29ydG1lbnQiLCJzdXBwb3J0LWZ1bm5lbCIsInJlc3RhdXJhbnQtYXBpIiwidG9wdXAtc2VydmljZSIsImludGVncmF0aW9uLWNvbmZpZy1zZXJ2aWNlIiwid29sdGF1dGgiLCJjb252ZXJzZS13aWRnZXQtY29uc3VtZXIiLCJhZC1pbnNpZ2h0cyIsInBheW1lbnQtc2VydmljZSIsInZlbnVlLWNvbnRlbnQtYXBpIiwibG95YWx0eS1nYXRld2F5IiwibG95YWx0eS1wcm9ncmFtLWFwaSIsIm9yZGVyLXhwIl0sImlzcyI6IndvbHRhdXRoIiwianRpIjoiZDZhNDliNGNlZDUwMTFlZTliZmM5NmQ2MjgxMTM2ODMiLCJ1c2VyIjp7ImlkIjoiNWRlZTE0MTBjMTRkMjcyMDhkOGYyZTc4IiwibmFtZSI6eyJmaXJzdF9uYW1lIjoiTmlrb2xhIiwibGFzdF9uYW1lIjoiRHJhZ29taXJvdmljIn0sImVtYWlsIjoibmlrb2xhZHJhZ29taXJvdmljQGljbG91ZC5jb20iLCJyb2xlcyI6WyJ1c2VyIl0sImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaG9uZV9udW1iZXJfdmVyaWZpZWQiOnRydWUsImNvdW50cnkiOiJTUkIiLCJsYW5ndWFnZSI6ImVuIiwicGVybWlzc2lvbnMiOltdLCJwaG9uZV9udW1iZXIiOiIrMzgxNjEyMTE4ODc3In0sImlhdCI6MTcxMTY2NDE5NCwiZXhwIjoxNzExNjY1OTk0LCJhbXIiOlsiZW1haWwiXX0.imQaTp-jgElAJFh0UxEFkHHYhq9yzr-siCvzlvEPrnXURDvFuwnv9f0Jr0p3pIq2U9XHB-ih61d802LppFn-BQ'
+        }
+    }
 
-glovo_headers = {
-    'Glovo-Delivery-Location-Longitude': '19.8467405',
-    'Glovo-Delivery-Location-Latitude': '45.26694579999999',
-    'Glovo-Location-City-Code': 'QND',
-    'Glovo-Delivery-Location-Timestamp': '1711636608028',
-    'Glovo-Delivery-Location-Accuracy': '0',
-    'Glovo-App-Platform': 'web'
+    glovo = {
+        'url': 'https://api.glovoapp.com/v3/feeds/categories/1',
+        'headers': {
+            'Glovo-Delivery-Location-Longitude': '19.8335496',
+            'Glovo-Delivery-Location-Latitude': '45.252317',
+            'Glovo-Location-City-Code': 'QND',
+            'Glovo-Delivery-Location-Timestamp': '1711636608028',
+            'Glovo-Delivery-Location-Accuracy': '0',
+            'Glovo-App-Platform': 'web'
+        }
+    }
 
-}
+    glovo_response = requests.get(glovo['url'], headers=glovo["headers"])
+    glovo_data = glovo_response.json()
 
-wolt_params = {
-    'lat': '45.26702450882689',
-    'lon': '19.846813157200813',
-}
+    wolt_response = requests.get(wolt["url"], params=wolt["params"], headers=wolt["headers"])
+    wolt_data = wolt_response.json()
 
-glovo_response = requests.get(glovo_url, headers=glovo_headers)
-glovo_data = glovo_response.json()
+    glovo_parsed_data = parse(glovo_data, "glovo")
+    wolt_parsed_data = parse(wolt_data, "wolt")
 
-wolt_response = requests.get(wolt_url, params=wolt_params)
-wolt_data = wolt_response.json()
+    output_data = merge([glovo_parsed_data, wolt_parsed_data])
 
-glovo_parsed_data = parse(glovo_data, "glovo")
-wolt_parsed_data = parse(wolt_data, "wolt")
+    return output_data
 
-output_data = merge(glovo_parsed_data, wolt_parsed_data)
-
-db_insert(output_data)
+db_insert(get_data())
