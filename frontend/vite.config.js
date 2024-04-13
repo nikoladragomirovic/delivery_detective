2735c9d97650e7d86ad29fccdddb5ade065052b9
Nikola Dragomirović <132782535+nikoladragomirovic@users.noreply.github.com>
2024-04-13 16:33:04 +0200

    Changed development config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+  },
+  open: (process.env.BROWSER = "chromium"),
   plugins: [react(), tailwindcss()],
 });
